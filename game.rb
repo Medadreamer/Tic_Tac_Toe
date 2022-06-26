4b54bc3e0d237cb34761e41c60ca46a40a9dbd74
Youssef <choukriyoussef7905@gmail.com>
2022-06-27 00:31:41 +0100

    Create game board class

--- game.rb
+++ game.rb
@@ -6,6 +6,20 @@ class Players
   end
 end
 
+class GameBoard
+  attr_accessor :board
+  def initialize
+    @board = [
+      ['_', '_', '_'],
+      ['_', '_', '_'],
+      ['_', '_', '_']
+    ]
+  end
+end
+
 player = Players.new("Perzival", "X")
 puts player.username
 puts player.weapon
+
+board = GameBoard.new
+p board.board
